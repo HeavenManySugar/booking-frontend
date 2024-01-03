2140f261d7d2d611325e6f0b736c2ce1c9c33c2a
ZRE <chy853@gmail.com>
2024-01-03 16:31:23 +0800

    btn top fade in out

--- Final/static/js/btn_top.js
+++ Final/static/js/btn_top.js
@@ -3,7 +3,7 @@
     //Get the button
     let mybutton = document.getElementById("btn-back-to-top");
 
-    // When the user scrolls down 20px from the top of the document, show the button
+    // When the user scrolls down 20px from the top of the document, show the button with fade-in effect
     window.onscroll = function () {
         scrollFunction();
     };
@@ -13,9 +13,9 @@
             document.body.scrollTop > 20 ||
             document.documentElement.scrollTop > 20
         ) {
-            mybutton.style.display = "block";
+            $(mybutton).fadeIn();
         } else {
-            mybutton.style.display = "none";
+            $(mybutton).fadeOut();
         }
     }
     // When the user clicks on the button, scroll to the top of the document
