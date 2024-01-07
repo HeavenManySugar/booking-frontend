3a9a5ba6ba103740e909263b3a120a9e4776d5bb
ZRE <chy853@gmail.com>
2024-01-08 01:22:16 +0800

    0108

--- Final/app.py
+++ Final/app.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_wtf import FlaskForm
-import psycopg2
-from wtforms import StringField, SubmitField, DateField, SelectField
+from wtforms import StringField, SubmitField, DateField, SelectField, EmailField, TelField
 from wtforms.validators import DataRequired, ValidationError
 
 from flask import Flask, render_template, request, redirect, url_for
@@ -11,7 +10,7 @@ from datetime import datetime
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YourSecretKey'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:0000@localhost/my_hotel'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/hotel'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
@@ -22,30 +21,31 @@ rooms = [
     ('102', 'Double Room'),
     ('103', 'Deluxe Room')
 ]
+
 class Guest(db.Model):
+    __tablename__ = 'guest'
     guest_id = db.Column(db.Integer, primary_key=True)
     guest_name = db.Column(db.String(255), nullable=False)
-    contact_info = db.Column(db.String(255))
-
-class Room(db.Model):
-    room_number = db.Column(db.String(10), primary_key=True)
-    # other room fields...
+    contact_email = db.Column(db.String(255), nullable=False)
+    contact_phone = db.Column(db.String(255), nullable=False)
 
 class Booking(db.Model):
+    __tablename__ = 'booking'
     booking_id = db.Column(db.Integer, primary_key=True)
     guest_id = db.Column(db.Integer, db.ForeignKey('guest.guest_id'), nullable=False)
-    room_number = db.Column(db.Integer, db.ForeignKey('room.room_number'), nullable=False)
     check_in_date = db.Column(db.Date, nullable=False)
     check_out_date = db.Column(db.Date, nullable=False)
-    # other booking fields...
+    total_price = db.Column(db.Float, nullable=False)
+
+    guest = db.relationship('Guest', backref='bookings')
 
 class BookingForm(FlaskForm):
-
     guest_name = StringField('Guest Name', validators=[DataRequired()])
     room_number = SelectField('Room Number', choices=rooms, validators=[DataRequired()])
     check_in_date = DateField('Check-In Date', format='%Y-%m-%d', validators=[DataRequired()])
     check_out_date = DateField('Check-Out Date', format='%Y-%m-%d', validators=[DataRequired()])
-    contact_info = StringField('Contact Information', validators=[DataRequired()])
+    contact_email = EmailField('Contact Email', validators=[DataRequired()])
+    contact_phone = TelField('Contact Phone', validators=[DataRequired()])
     submit = SubmitField('Book Now')
     def validate_check_in_date(self, field):
         if field.data >= self.check_out_date.data:
@@ -59,13 +59,12 @@ def index():
 def booking():
     form = BookingForm()
     if form.validate_on_submit():
-        new_guest = Guest(guest_name=form.guest_name.data, contact_info=form.contact_info.data)
+        new_guest = Guest(guest_name=form.guest_name.data, contact_email=form.contact_email.data, contact_phone=form.contact_phone.data)
         db.session.add(new_guest)
         db.session.flush()  # Flush to get the ID of the new guest
 
         new_booking = Booking(
             guest_id = new_guest.guest_id,
-            room_number = form.room_number.data,
             check_in_date = form.check_in_date.data,
             check_out_date = form.check_out_date.data
         )
@@ -74,6 +73,14 @@ def booking():
         
         return redirect(url_for('index'))
     return render_template('booking.html', form=form)
+
+@app.route('/rooms')
+def rooms():
+    return render_template('rooms.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Final/static/js/btn_top.js
+++ Final/static/js/btn_top.js
@@ -1,7 +1,13 @@
 (() => {
-    'use strict'
+    'use strict'    
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+
     //Get the button
-    let mybutton = document.getElementById("btn-back-to-top");
+    var mybutton = document.getElementById("btn-back-to-top");
+    var tooltip = bootstrap.Tooltip.getInstance(mybutton)
 
     // When the user scrolls down 20px from the top of the document, show the button with fade-in effect
     window.onscroll = function () {
@@ -16,13 +22,7 @@
             $(mybutton).fadeIn();
         } else {
             $(mybutton).fadeOut();
+            $(tooltip).hide();
         }
-    }
-    // When the user clicks on the button, scroll to the top of the document
-    mybutton.addEventListener("click", backToTop);
-
-    function backToTop() {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
     }
 })()
