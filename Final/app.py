f14008fa4adabfedfe4a308f5e342056cb085a25
ZRE <chy853@gmail.com>
2024-01-08 21:50:52 +0800

    0108 ver1

--- Final/app.py
+++ Final/app.py
@@ -82,5 +82,9 @@ def rooms():
 def about():
     return render_template('about.html')
 
+@app.route('/test')
+def test():
+    return render_template('test.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
